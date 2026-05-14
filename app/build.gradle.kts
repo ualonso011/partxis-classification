48b96ed9ea44674f8bc0746b489baecbe41261c0
ualonso011 <ugaitzalonso@gmail.com>
2026-05-14 16:45:08 +0200

    fix: migrate kotlinOptions to compilerOptions DSL with JvmTarget

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,17 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
     id("org.jetbrains.kotlin.plugin.compose")
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
 }
 
 android {
@@ -36,10 +44,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlin {
-        jvmTarget.set(JavaVersion.VERSION_17)
     }
 
     buildFeatures {
