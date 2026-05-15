935815a83925fe0211e4fa12e1f14cb890ebfef7
ualonso011 <ugaitzalonso@gmail.com>
2026-05-15 19:26:56 +0200

    fix: simplify to debug build only

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,19 +32,6 @@ android {
         }
     }
 
-    if (project.hasProperty("KEYSTORE_PATH")) {
-        val keystoreFile = file(project.property("KEYSTORE_PATH").toString())
-        val keystorePassword = project.property("KEYSTORE_PASSWORD").toString()
-        val keystoreAlias = project.property("KEYSTORE_ALIAS").toString()
-
-        signingConfigs.create("release") {
-            storeFile = keystoreFile
-            storePassword = keystorePassword
-            keyAlias = keystoreAlias
-            keyPassword = keystorePassword
-        }
-    }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -52,9 +39,6 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            if (project.hasProperty("KEYSTORE_PATH")) {
-                signingConfig = signingConfigs.getByName("release")
-            }
         }
     }
 
